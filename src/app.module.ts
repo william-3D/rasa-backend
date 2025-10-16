3e957a0b45ef35f77baafeed629e7d481158eb30
Sonia Personal <98wsonia@gmail.com>
2025-10-16 16:53:46 +0800

    chore: setup API flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfileModule } from './profile/profile.module';
+import { ConditionsModule } from './conditions/conditions.module';
+import { AllergiesModule } from './allergies/allergies.module';
 
 @Module({
-  imports: [],
+  imports: [ProfileModule, ConditionsModule, AllergiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$disconnect();
+  }
+}
