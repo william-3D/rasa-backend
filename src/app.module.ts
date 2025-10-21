7c8c87dfa58cfa475a513ddd830b6938a96ba305
Sonia Personal <98wsonia@gmail.com>
2025-10-21 15:07:57 +0800

    feat: add recipe and scraper modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfileModule } from './profile/profile.module';
 import { ConditionsModule } from './conditions/conditions.module';
 import { AllergiesModule } from './allergies/allergies.module';
+import { RecipesModule } from './recipes/recipes.module';
+import { ScraperModule } from './scraper/scraper.module';
 
 @Module({
-  imports: [ProfileModule, ConditionsModule, AllergiesModule],
+  imports: [
+    ScheduleModule.forRoot(),ProfileModule, ConditionsModule, AllergiesModule, RecipesModule, ScraperModule],
   controllers: [AppController],
   providers: [AppService],
 })
