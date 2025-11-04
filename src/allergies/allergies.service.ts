9db2407746bad1a0c91f705dce2608924d0f94af
Sonia Personal <98wsonia@gmail.com>
2025-11-04 17:27:40 +0800

    add: support for HTTP methods besides GET

--- src/allergies/allergies.service.ts
+++ src/allergies/allergies.service.ts
@@ -13,4 +13,23 @@ export class AllergiesService {
       },
     });
   }
+
+  async createAllergy(name: string) {
+    return this.prisma.allergy.create({
+      data: { name },
+    });
+  }
+
+  async updateAllergy(id: string, name: string) {
+    return this.prisma.allergy.update({
+      where: { id },
+      data: { name },
+    });
+  }
+
+  async deleteAllergy(id: string) {
+    return this.prisma.allergy.delete({
+      where: { id },
+    });
+  }
 }
--- src/conditions/conditions.service.ts
+++ src/conditions/conditions.service.ts
@@ -13,4 +13,23 @@ export class ConditionsService {
       },
     });
   }
+
+  async createCondition(name: string) {
+    return this.prisma.condition.create({
+      data: { name },
+    });
+  }
+
+  async updateCondition(id: string, name: string) {
+    return this.prisma.condition.update({
+      where: { id },
+      data: { name },
+    });
+  }
+
+  async deleteCondition(id: string) {
+    return this.prisma.condition.delete({
+      where: { id },
+    });
+  }
 }
